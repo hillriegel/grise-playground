dfc11e9db8fe5d6459b75c305a6fffb76d716c42
GRISECON HILLRIEGEL <hillriegel@gmail.com>
2024-05-23 15:32:27 -0700

    properly use debounce and useCallback in the fetch

--- app/CustomComponents/currencyConverter/hooks/useExchangeRate.tsx
+++ app/CustomComponents/currencyConverter/hooks/useExchangeRate.tsx
@@ -1,15 +1,12 @@
-'use client'
-
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { debounce } from 'lodash';
 
-export const useExchangeRate = (currencyFrom: string, currencyTo:string, amountFrom: string) => {
+export const useExchangeRate = (currencyFrom: string, currencyTo: string, amountFrom: string) => {
   const [amountTo, setAmountTo] = useState('0.00');
-
   const authKey = process.env.NEXT_PUBLIC_X_RAPID_API_KEY;
 
-  const fetchExchangeRate = useCallback(debounce((currencyFrom, currencyTo, amountFrom) => {
+  const fetchExchangeRate = useCallback((currencyFrom: string, currencyTo: string, amountFrom: string) => {
     axios.get('https://currency-exchange.p.rapidapi.com/exchange', {
       headers: {
         'X-RapidAPI-Key': authKey,
@@ -21,18 +18,20 @@ export const useExchangeRate = (currencyFrom: string, currencyTo:string, amountF
         q: amountFrom
       }
     }).then(response => {
-      let calculatedAmount = Math.round((parseFloat(amountFrom) * response.data) * 100) / 100;
+      const calculatedAmount = Math.round((parseFloat(amountFrom) * response.data) * 100) / 100;
       setAmountTo(calculatedAmount.toString());
     }).catch(error => {
       console.error('Error fetching exchange rate:', error);
     });
-  }, 500), []);
+  }, [authKey]);  // Add any other dependencies here if they are supposed to change over time
+
+  const debouncedFetchExchangeRate = useMemo(() => debounce(fetchExchangeRate, 500), [fetchExchangeRate]);
 
   useEffect(() => {
     if (amountFrom && currencyFrom && currencyTo) {
-      fetchExchangeRate(currencyFrom, currencyTo, amountFrom);
+      debouncedFetchExchangeRate(currencyFrom, currencyTo, amountFrom);
     }
-  }, [currencyFrom, currencyTo, amountFrom, fetchExchangeRate]);
+  }, [currencyFrom, currencyTo, amountFrom, debouncedFetchExchangeRate]);
 
   return [amountTo];
 };
