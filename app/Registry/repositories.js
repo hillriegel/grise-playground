d5bb3ed7c8d45358c05d0d64996a21a89ec697ec
Grisecon Hillriegel <hillriegel@gmail.com>
2024-06-13 11:49:11 -0700

    update repositories data

--- app/Registry/repositories.js
+++ app/Registry/repositories.js
@@ -1,6 +1,11 @@
 export const REPOSITORIES = [
     {
-        name: grisecon-redux-cart,
+        name: 'grisecon-redux-cart',
+        url:'https://github.com/hillriegel/grisecon-redux-cart/tree/main/src',
+        tags: ['redux', 'shopping cart', 'slice', 'react', 'configureStore', 'reducer', 'useDispatch', 'useSelector' ]
+    },
+    {
+        name: 'react-tanstack',
         url:'https://github.com/hillriegel/grisecon-redux-cart/tree/main/src',
         tags: ['redux', 'shopping cart', 'slice', 'react', 'configureStore', 'reducer', 'useDispatch', 'useSelector' ]
     }
